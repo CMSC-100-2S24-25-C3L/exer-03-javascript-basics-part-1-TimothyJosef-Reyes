45b554863a3ae46d839f1cb4133d726bff4e93f9
TJ Reyes <tareyes1@up.edu.ph>
2025-02-20 14:23:55 +0800

    test: added more tests to futher check accuracy

--- index.js
+++ index.js
@@ -66,7 +66,8 @@ console.log(validatePassword("hello", "hello"));
 console.log(validatePassword("hello1234", "hello1234"));
 console.log(validatePassword("Hello1234", "Hello1234"));
 console.log(validatePassword("HELLO1234", "HELLO1234"));
-console.log(reversePassword("Hello"));
+console.log(reversePassword("HelloWorld"));
 console.log(storePassword("John", "Pass1234", "Pass1234"));
+console.log(storePassword("John", "Pass123", "Pass123"));
 console.log(storePassword("John", "Pass123", "Pass12345"));
 
