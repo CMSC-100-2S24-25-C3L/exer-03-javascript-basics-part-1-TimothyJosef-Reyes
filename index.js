19570592e9b79dfcf2efca439a884fa55476fbb7
TJ Reyes <tareyes1@up.edu.ph>
2025-02-20 14:12:04 +0800

    feat: added storePassword function

--- index.js
+++ index.js
@@ -48,9 +48,25 @@ function reversePassword(password){
     return reversed;
 }
 
+function storePassword(name, password1, password2){
+    let output = new Object();
+    output["name"] = name;
+
+    if(validatePassword(password1, password2)){
+        output["newpassword"] = reversePassword(password1);
+    } else {
+        output["newpassword"] = password1;
+    }
+
+    return output;
+}
+
 console.log(validatePassword("helloworld", "hello"));
 console.log(validatePassword("hello", "hello"));
 console.log(validatePassword("hello1234", "hello1234"));
 console.log(validatePassword("Hello1234", "Hello1234"));
 console.log(validatePassword("HELLO1234", "HELLO1234"));
 console.log(reversePassword("Hello"));
+console.log(storePassword("John", "Pass1234", "Pass1234"));
+console.log(storePassword("John", "Pass123", "Pass12345"));
+
